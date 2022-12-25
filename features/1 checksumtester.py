b561a13099f598af3d32aae6e516a66ed90209f4
jumpie700 <jumpie_somerandomnumbers@protonmail.com>
2022-12-25 21:40:52 +0100

    hate my life why are the ignored ones in the list? v2

--- features/1 checksumtester.py
+++ features/1 checksumtester.py
@@ -1,21 +1,35 @@
 import os
-def getListOfFiles(dirName="./",ignored_dirs=[".git",".idea"]):
-    listOfFile = os.listdir(dirName)
-    allFiles = []
-    for entry in listOfFile:
-        fullPath = os.path.join(dirName, entry)
-        if os.path.isdir(fullPath):
-            allFiles = allFiles + getListOfFiles(fullPath)
-        else:
-            allFiles.append(fullPath)
-    all_wanted_files = []
-    # check if file is in an ignored dir:
-    for file in allFiles:
-        for dir in ignored_dirs:
-            if dir in file:
-                print(file)
-                continue
-            else:
-                all_wanted_files.append(file)
-    return all_wanted_files
-print(getListOfFiles())
+from hashlib import sha512
+import requests
+def _search(folder_path: str = "./", ignored_dirs: list = [".git",".idea"]):# thx ice :D
+    file_list = []
+    for path, subdirs, files in os.walk(folder_path, topdown=True):
+        if not any(ignore in path for ignore in ignored_dirs):
+            for file in files:
+                file_list.append(os.path.join(path, file))
+
+    return file_list
+
+def get_sum(files):
+    sums = []
+    for file in files:
+        with open(file,"r") as opened_file:
+            content = opened_file.read()
+            hash = sha512(content.encode())
+            sum = hash.hexdigest()
+            #print(f"{file} : {sum}\n")
+            sums.append(sum)
+
+    project_sum = ""
+    for sum in sums:
+        project_sum = project_sum + sum
+    #print(project_sum)
+    return project_sum
+def compare_sum(sum):
+    original_sum = requests.get("https://raw.githubusercontent.com/FelixSiegel/J-I-B/main/checksum.txt").text
+    print(original_sum+"\n"+sum)
+    if sum == original_sum:
+        return True
+    else:
+        return False
+print(compare_sum(get_sum(_search())))
